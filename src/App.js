e60874883de74c25f0caf9fb6b00c4b03b25b361
WhatsWrongOB <148737556+WhatsWrongOB@users.noreply.github.com>
2023-12-29 17:15:01 +0500

    project setup

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import { lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Loader from './Components/Loader/Loader'
 const Home = lazy(() => import('./Pages/Home/Home'))
-const Navbar = lazy(() => import('./Components/Navbar/Navbar'))
 const Cart = lazy(() => import('./Pages/Cart/Cart'))
+const LogIn = lazy(() => import('./Pages/Login/Login'))
+
 
 const App = () => {
   return (
 
     <Router>
       <Suspense fallback={<Loader />}>
-        <Navbar />
         <Routes>
           <Route exact path='/' element={<Home />} />
           <Route exact path='/cart' element={<Cart />} />
+          <Route exact path='/Login' element={<LogIn />} />
+          <Route exact path="*" element={<h1>404 Page Not Found</h1>} />
         </Routes>
       </Suspense>
     </Router>

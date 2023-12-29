69fdb7130609fb39406011b31f65715511f8b67c
WhatsWrongOB <148737556+WhatsWrongOB@users.noreply.github.com>
2023-12-29 20:22:14 +0500

    project-setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Loader from './Components/Loader/Loader'
+const LogIn = lazy(() => import('./Pages/Login/Login'))
 const Home = lazy(() => import('./Pages/Home/Home'))
 const Cart = lazy(() => import('./Pages/Cart/Cart'))
-const LogIn = lazy(() => import('./Pages/Login/Login'))
+const Men = lazy(() => import('./Pages/Men/Men'))
+const Women = lazy(() => import('./Pages/Women/Women'))
+
 
 
 const App = () => {
@@ -12,9 +15,11 @@ const App = () => {
     <Router>
       <Suspense fallback={<Loader />}>
         <Routes>
+          <Route exact path='/Login' element={<LogIn />} />
           <Route exact path='/' element={<Home />} />
           <Route exact path='/cart' element={<Cart />} />
-          <Route exact path='/Login' element={<LogIn />} />
+          <Route exact path='/men' element={<Men />} />
+          <Route exact path='/women' element={<Women />} />
           <Route exact path="*" element={<h1>404 Page Not Found</h1>} />
         </Routes>
       </Suspense>
---
+++ src/Pages/Men/Men.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Men.css'
+
+
+const Men = () => {
+  return (
+    <div>Men</div>
+  )
+}
+
+export default Men
